3c92efaf6a2618ccc24fd455524a20c52f31ae75
ArtIPok <apokora@itsap.pl>
2020-12-15 23:23:21 +0100

    Generate links in article

--- js/script.js
+++ js/script.js
@@ -43,7 +43,8 @@
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector= '.post-tags .list';
 
   function generateTitleLinks(){
     // console.log(generateTitleLinks);
@@ -91,4 +92,48 @@
 
   }
   generateTitleLinks();
-}    
+
+  function generateTags(){
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    /* START LOOP: for every article: */
+    for(let article of articles){
+      /* find tags wrapper */
+      const titleList = article.querySelector(optArticleTagsSelector);
+      console.log(titleList);
+
+      /* make html variable with empty string */
+      let html = '';
+
+      /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log(articleTags);
+
+      /* split tags into array */
+      const articleTagsArray = articleTags.split(' ');
+      console.log(articleTagsArray);
+
+      /* START LOOP: for each tag */
+      for(let tag of articleTagsArray){
+        console.log(tag);
+
+        /* generate HTML of the link */
+        const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
+
+        /* add generated code to the html variable */
+        html = html + linkHTML;
+        console.log(html);
+
+      /* END LOOP: for each tag */
+      }
+
+      /* insert HTML of all the links into the tags wrapper */
+      titleList.innerHTML = html;
+      console.log(titleList);
+
+    /* END LOOP: for every article: */
+  }
+  }
+  generateTags();
+}
